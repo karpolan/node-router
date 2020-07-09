c7a1cd1c64c64d52df7d829b5d93421c693c9c2d
Anton Karpenko <karpolan@karpolan.com>
2020-07-09 14:46:13 +0200

    feat: get method

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const http = require('http');
 
-const router = {};
+const router = [];
 
 // Create an instance of the http server to handle HTTP requests
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('All good!');
 });
+
+app.prototype.get = function (path, func) {
+  console.log(`get(${path})`);
+  router.push({
+    path: path,
+    method: 'GET',
+    handle: func,
+  });
+};
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
